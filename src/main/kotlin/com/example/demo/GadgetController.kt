e4b70e502ea81dbfd92147eee5d54a46fda679af
mathewnicholls <mathewnicholls@protonmail.com>
2021-09-24 16:05:48 +0100

    fix delete endpoint

--- src/main/kotlin/com/example/demo/GadgetController.kt
+++ src/main/kotlin/com/example/demo/GadgetController.kt
@@ -55,7 +55,7 @@ class GadgetController(private val repo: GadgetRepo) {
         }.orElse(ResponseEntity<Gadget>(HttpStatus.INTERNAL_SERVER_ERROR))
     }
 
-    @DeleteMapping
+    @DeleteMapping("/gadgets/{id}")
     fun removeGadgetById(@PathVariable("id") gadgetId: Long): ResponseEntity<Void> {
          val gadget = repo.findById(gadgetId)
         if (gadget.isPresent) {
